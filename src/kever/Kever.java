a007f8dba71293d84f6b5c217346905ba6d63335
Vén Dávid Zsolt (SZF_N_2025_2027) <ven.david.zsolt@diak.szamalk-szalezi.hu>
2026-09-23 15:51:21 +0200

    daviresze

--- src/kever/Kever.java
+++ src/kever/Kever.java
@@ -1,22 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package kever;
 
-/**
- *
- * @author AmmerMárkDániel(SZF_
- */
 public class Kever {
 
-    /**
-     * @param args the command line arguments
-     */
+
     
     static String[] pakli = new String[22];
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
         private static void kever(int oszlop) {
             String[] uj = new String[22];
@@ -29,6 +20,20 @@ public class Kever {
                     uj[sor+7] = pakli[19 - (sor - 1) * 3];
                     uj[sor+14] = pakli[21 - (sor - 1) * 3];
                 }
+                break;
+                case 2:
+                    for (int sor = 1; sor <= 7; sor++) {
+                    uj[sor] = pakli[19 - (sor - 1) * 3];
+                    uj[sor+7] = pakli[20 - (sor - 1) * 3];
+                    uj[sor+14] = pakli[21 - (sor - 1) * 3];
+                    }
+                    break;
+                case 3:
+                    for (int sor = 1; sor <= 7; sor++) {
+                    uj[sor] = pakli[20 - (sor - 1) * 3];
+                    uj[sor+7] = pakli[21 - (sor - 1) * 3];
+                    uj[sor+14] = pakli[19 - (sor - 1) * 3];
+                    }
                 break;
                 
                 default:
